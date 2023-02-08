92f3960a39213194b748351e384c952fff33effe
shwetabh <shwetabh.srivastava@gmail.com>
2023-02-08 19:29:51 +0530

    sddkfffggk

--- backend/server.js
+++ backend/server.js
@@ -32,7 +32,7 @@ const path = require("path");
 
 if (process.env.NODE_ENV === "production") {
  // app.use(express.static(path.join(_dirname,"../frontend/build")));
-  app.use(express.static("/frontend/build"));
+  app.use(express.static("../frontend/build"));
   
   app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "../frontend/build/index.html"));
