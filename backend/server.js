31484746e4d60727e22aded8a48e800b6d16bdf7
shwetabh <shwetabh.srivastava@gmail.com>
2023-02-09 08:31:49 +0530

    dseddssss

--- backend/server.js
+++ backend/server.js
@@ -65,7 +65,7 @@ app.post("/create-payment-intent", async (req, res) => {
   // Create a PaymentIntent with the order amount and currency
   const paymentIntent = await stripe.paymentIntents.create({
     amount: calculateOrderAmount(items),
-    currency: "inr",
+    currency: "usd",
     automatic_payment_methods: {
       enabled: true,
     },
