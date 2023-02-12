178aaaa64f5e61b18e3ec4fbedb96266cffe6e0e
shwetabh <shwetabh.srivastava@gmail.com>
2023-02-12 16:17:47 +0530

    sdfrferdsdfdderwerrddewasdsdsaewrllsdal

--- frontend/src/components/header/Header.js
+++ frontend/src/components/header/Header.js
@@ -113,7 +113,7 @@ const Header = () => {
    
         <div className={styles.header}>
           {logo}
-          <img src="/images/1.jpeg" />
+          <img src="/images/1.jpg" alt="Girl in a jacket" width="500" height="600"></img>
           <nav>
             <ul >
               <li className={styles["logo-mobile"]}>
