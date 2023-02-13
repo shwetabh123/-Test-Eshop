791fd472e7b627a0461c7f9f76bcb94bd75e56b3
shwetabh <shwetabh.srivastava@gmail.com>
2023-02-13 19:51:31 +0530

    oooiDDLLDFG

--- frontend/src/components/checkoutForm/CheckoutForm.js
+++ frontend/src/components/checkoutForm/CheckoutForm.js
@@ -192,7 +192,7 @@ const CheckoutForm = () => {
           transactionInfo: {
             totalPriceStatus: "FINAL",
             totalPriceLabel: "Total",
-            totalPrice: cartTotalAmount.toFixed(2) ,
+            totalPrice: price * cartQuantity ,
             currencyCode: "inr",
             countryCode: "IN",
           },
