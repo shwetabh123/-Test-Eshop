d1f33eb15d3d9b5540d7542affc196aa66b334f8
shwetabh <shwetabh.srivastava@gmail.com>
2023-02-23 09:04:29 +0530

    dff

--- frontend/src/pages/cancellationrefund/Cancellationrefund.js
+++ frontend/src/pages/cancellationrefund/Cancellationrefund.js
@@ -19,15 +19,15 @@ const Cancellationrefund = () => {
                 <Card cardClass={styles.card2}>
                   <h2>Return Policy:</h2>
                  
-                  <p >Customers can initiate return using  Website, and Mobile Site.</p>
-                 < p  >Customers can initiate return within 30 days from Order Delivery.</p>
+                  <p >1.Customers can initiate return using  Website, and Mobile Site.</p>
+                 < p >2.Customers can initiate return within 30 days from Order Delivery.</p>
 
            
 
                 <h2>Cancellation Policy:</h2>
             
-                  <p>You can cancel an order by clicking on the Cancel link/button.</p>
-                  <p >You will be able to cancel an order till the time it is dispatched from the seller. In case you are unable to cancel an order from the app or website, you can reject the order at doorstep.</p>
+                  <p>1.You can cancel an order by clicking on the Cancel link/button.</p>
+                  <p >2.You will be able to cancel an order till the time it is dispatched from the seller. In case you are unable to cancel an order from the app or website, you can reject the order at doorstep.</p>
 
 
                 <h2>Refund Policy:</h2>
@@ -37,10 +37,10 @@ const Cancellationrefund = () => {
           
                 <h3>Refund against Return:</h3>
 
-                  <p>In case of Cash on Delivery (COD) orders, customers can choose to get their refund on Return(s) through one of the below options. The refund shall be initiated within 5 days from the date of return pickup.</p>
-                  <p>Refund credited to the Bank Account entered by the customer at the time of initiating the return request.</p>
-                  <p>In case the payment is made by Credit/Debit Card, Net Banking or Third Party Wallet, you may get the refund within 7 to 15 working days. Kindly note that there may be delays in refund as they're handled by the Bank.</p>
-                  <p>In case of any query related to refund, please call our customer service desk at 7019048203 or email us at Support@eshop.com</p>
+                  <p>1.In case of Cash on Delivery (COD) orders, customers can choose to get their refund on Return(s) through one of the below options. The refund shall be initiated within 5 days from the date of return pickup.</p>
+                  <p>2.Refund credited to the Bank Account entered by the customer at the time of initiating the return request.</p>
+                  <p>3.In case the payment is made by Credit/Debit Card, Net Banking or Third Party Wallet, you may get the refund within 7 to 15 working days. Kindly note that there may be delays in refund as they're handled by the Bank.</p>
+                  <p>4.In case of any query related to refund, please call our customer service desk at 7019048203 or email us at Support@eshop.com</p>
                 
                 </Card>
               </div>
